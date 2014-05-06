ab99521a01c2fc2e1996d0cbaecaf0d69a4d1c7e
kevin <kevin8096@live.com>
2014-05-06 18:41:18 +0800

    start

---
+++ code.py
@@ -0,0 +1,18 @@
+import web
+import os
+urls = (
+    '/test', 'hello',
+    '/test2', 'hi',
+    )
+class hello:
+    def __init__(self):
+        self.render = web.template.render(os.path.join(os.path.dirname(__file__), 'templates/'))
+        
+    def GET(self,name=None):
+        return self.render.index('kevin')
+
+class hi:
+    def GET(self):
+	return " hi"
+
+application = web.application(urls, globals()).wsgifunc()
